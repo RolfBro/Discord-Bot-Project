f52b6abb0c5dc2e796852bad2986e1a616aa4ecb
RolfBro <65670485+RolfBro@users.noreply.github.com>
2023-05-23 05:07:20 +0000

    Joining functionality of bot works

--- index.js
+++ index.js
@@ -54,7 +54,8 @@ client.on("messageCreate", message => {
     // Create the party
     const party = {
       creator: message.author.tag,
-      members: [message.author.tag],
+      creatorAvatarURL: message.author.displayAvatarURL(),
+      members: [{tag: message.author.tag, id: message.author.id}],
       size: partySize
     };
 
@@ -63,18 +64,19 @@ client.on("messageCreate", message => {
 
     // Create the party description
     let partyDescription = '';
-    for (let i = 0; i < partySize; i++) {
-      if (i < party.members.length) {
-        partyDescription += `${i+1}. ${party.members[i]}\n`;
+    for (let i = 0; i < party.size; i++) {
+      if(i < party.members.length) {
+        partyDescription += `${i+1}. ${party.members[i].tag}\n`;
       } else {
-        partyDescription += `${i+1}. \n`;
+          partyDescription += `${i+1}.\n`;
       }
     }
 
     const embed = new EmbedBuilder()
+      .setColor('Red')
       .setTitle(game)
       .setDescription(partyDescription)
-      .setFooter({text: `Party created by ${message.author.tag}`});
+      .setFooter({text: `Party created by ${message.author.tag}`, iconURL:                        message.author.displayAvatarURL()});
 
     message.channel.send({ embeds: [embed] });
   }
@@ -100,6 +102,65 @@ client.on("messageCreate", message => {
     // Delete the party
     parties.delete(game);
     message.channel.send(`Party titled '${game}' has been cancelled`);
+  }
+
+  // Join a party
+  if (args[0] === "!lgn" && args[1] === "join") {
+    const game = args[2];
+
+    // Check if a party with the given title exists
+    if (!parties.has(game)) {
+      message.channel.send(`No party titled '${game}' found.`);
+      return;
+    }
+    
+    const party = parties.get(game);
+    
+    // Check if the user is already in the party
+    if (party.members.includes(message.author.tag)) {
+      message.channel.send('You are already in this party.');
+      return;
+    }
+
+    // Add the user to the party
+    party.members.push({tag: message.author.tag, id: message.author.id});
+
+    //Create the party Description
+    let partyDescription = '';
+    for (let i = 0; i < party.size; i++) {
+      if(i < party.members.length) {
+        partyDescription += `${i+1}. ${party.members[i].tag}\n`;
+      } else {
+          partyDescription += `${i+1}.\n`;
+      }
+    }
+
+    // If the party is not yet full, don't notify members yet
+    if (party.members.length < party.size) {
+      const embed = new EmbedBuilder()
+      .setColor('Red')
+      .setTitle(game)
+      .setDescription(partyDescription)
+      .setFooter({text: `Party created by ${party.creator}`,                           iconURL: party.creatorAvatarURL});
+
+      // Send the updated party embed
+      message.channel.send({ embeds: [embed] });
+    }
+
+    // If the party is now full, notify all members
+    if (party.members.length === party.size) {
+      const memberTags = party.members.map(member => `<@${member.id}>`).join(', ');
+      
+      const embed = new EmbedBuilder()
+      .setColor('Green')
+      .setTitle(game)
+      .setDescription(partyDescription)
+      .setFooter({text: `Party created by ${party.creator}`,                           iconURL: party.creatorAvatarURL});
+
+      // Send the updated party embed
+      message.channel.send({ embeds: [embed] });
+      message.channel.send(`The party '${game}' is now full. Members: ${memberTags}`);
+    }
   }
 })
 
